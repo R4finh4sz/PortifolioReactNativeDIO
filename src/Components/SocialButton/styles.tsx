528f107c92831e0af76a8b6700a751d01a9df373
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-03 19:06:49 -0300

    Feat: Added theme

--- src/Components/SocialButton/styles.tsx
+++ src/Components/SocialButton/styles.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import theme from '@/src/theme';
 
 export const Container = styled(TouchableOpacity)`
   width: 90%;
@@ -9,8 +10,7 @@ export const Container = styled(TouchableOpacity)`
   
   border-radius: 6px;
   border-width: 2px;
-  border-color: #333;
-
+  border-color: ${theme.COLORS.GRAY_100};
   flex-direction: row;
   align-items: center;
   justify-content: center;
@@ -19,14 +19,14 @@ export const Container = styled(TouchableOpacity)`
 `;
 
 export const Title = styled.Text`
-  font-size: 16px;
-  color: #333;
-  font-weight: bold;
+  font-size: ${theme.FONT_SIZE.MD};
+  color: ${theme.COLORS.GRAY_100};
+  font-family: ${theme.FONT_FAMILY.BOLD};
 `;
 
 export const Icon = styled(FontAwesome).attrs({
   size: 24,
-  color: '#333',
+  color: '#E1E1E6',
 })`
   margin-right: 12px;
 `;
